078f310a10b277aee69a36b6eac0c4f3f366cb2a
Xendy <xendyexe@gmail.com>
2025-03-16 12:03:30 -0700

    3/16/2025

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoToOrigin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoToOrigin.java
@@ -12,14 +12,19 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.limemode.PIDController;
 
 @Config
 @TeleOp(name="GoToOrigin")
 public class GoToOrigin extends LinearOpMode {
     public static double TARGET_X = 0;
     public static double TARGET_Y = 0;
-    public static double SPEED = 100;
-    public static double SPIN_SPEED = 200;
+    public static double SPEED = 2000;
+    public static double SPIN_SPEED = 1000;
+
+    public static double kP = 0.2;
+    public static double kI = 0;
+    public static double kD = 1;
     public MultipleTelemetry multipleTelemetry;
     public FtcDashboard dashboard;
     @Override
@@ -27,27 +32,50 @@ public class GoToOrigin extends LinearOpMode {
         dashboard = FtcDashboard.getInstance();
         multipleTelemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
         Chassis chassis = new Chassis(this);
+        PIDController xController = new PIDController(TARGET_X, kD, kI, kD, multipleTelemetry);
+        PIDController yController = new PIDController(TARGET_Y, kP, kI, kD, multipleTelemetry);
         waitForStart();
+        double lastX = 0;
+        double lastY = 0;
+        double lastTime = -1;
         while (opModeIsActive()) {
+            // Sync values
+
+            xController.sync(TARGET_X, kD, kI, kP);
+            yController.sync(TARGET_Y, kD, kI, kP);
+            multipleTelemetry.addLine("For graphing purposes");
+            multipleTelemetry.addData("Target Y", TARGET_Y);
             YawPitchRollAngles orientation = chassis.imu.getRobotYawPitchRollAngles();
             double yawRads = orientation.getYaw(AngleUnit.RADIANS);
             double yawDeg = orientation.getYaw(AngleUnit.DEGREES);
+
             LLResult result = chassis.getLimelightData(yawDeg);
             if (result != null) {
                 Position pos = result.getBotpose_MT2().getPosition();
                 double x_diff = TARGET_X - pos.x;
                 double y_diff = TARGET_Y - pos.y;
-                telemetry.addData("X Diff", x_diff);
-                telemetry.addData("Y Diff", y_diff);
-                telemetry.addData("Position", pos);
-                telemetry.addData("Speed", SPEED);
-                chassis.move(yawRads, x_diff, y_diff, 0, SPEED);
+                multipleTelemetry.addData("Current Y", pos.y);
+                multipleTelemetry.addData("Y Error", y_diff);
+
+                multipleTelemetry.addData("X Diff", x_diff);
+                multipleTelemetry.addData("Y Diff", y_diff);
+                multipleTelemetry.addData("Position", pos);
+                multipleTelemetry.addData("Speed", SPEED);
+
+                double x = xController.update(pos.x, false);
+                double y = yController.update(pos.y, true);
+                multipleTelemetry.addData("moveX", x);
+                multipleTelemetry.addData("moveY", y);
+                chassis.move(yawRads, x, y, 0, SPEED);
+                lastX = x;
+                lastY = y;
+                lastTime = System.currentTimeMillis();
             }
             else {
-                telemetry.addLine("Cannot see apriltags, spinning");
+                multipleTelemetry.addLine("Cannot see apriltags, spinning");
                 chassis.move(yawRads, 0, 0, 1, SPIN_SPEED);
             }
-            telemetry.update();
+            multipleTelemetry.update();
         }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limemode/PIDController.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.limemode;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class PIDController {
+    public double kP, kI, kD, target;
+    private double previousTime = -1;
+    private double lastError = -1;
+    private double integralError = 0;
+    private double actualError = 0;
+    private double averageError = 0;
+    public boolean firstInit = false;
+    private Telemetry telemetry;
+    public PIDController(double initalTarget, double kP, double kI, double kD, Telemetry debug) {
+        this.kP = kP;
+        this.telemetry = debug;
+        this.kI = kI;
+        this.kD = kD;
+        this.target = initalTarget;
+        firstInit = true;
+    }
+
+    public double update(double currentPos, boolean debug) {
+        double currentTime = System.currentTimeMillis();
+        double deltaTime = previousTime == -1 ? 0 : currentTime - previousTime;
+        deltaTime /= 1000;
+        double trueError = target - currentPos;
+        if (Math.abs(trueError - actualError) > 0.3 && !firstInit)
+            actualError = trueError + (trueError - actualError) * 0.1;
+        else actualError = trueError;
+        if (firstInit) {
+            averageError = actualError;
+            firstInit = false;
+        }
+        else {
+            averageError = averageError * 0.9 + actualError * 0.1;
+        }
+        double error = Math.abs(averageError) < 0.2 ? averageError : actualError;
+        double derivitiveError = deltaTime == 0 ? 0 : (error - lastError)/deltaTime;
+        integralError += error * deltaTime;
+
+        lastError = error;
+        previousTime = currentTime;
+        if (debug) telemetry.addData("err actual", error);
+        return (kP * error) + (kI * integralError) + (kD * derivitiveError);
+    }
+
+    public void sync(double target, double kD, double kI, double kP) {
+        if (this.kD != kD) { this.kD = kD; reset(); }
+        if (this.kI != kI) { this.kI = kI; reset(); }
+        if (this.kP != kP) { this.kP = kP; reset(); }
+        if (this.target != target) { this.target = target; reset(); }
+    }
+
+    public void reset() {
+        previousTime = 0;
+        lastError = 0;
+        integralError = 0;
+        firstInit = true;
+        actualError = 0;
+        averageError = 0;
+    }
+}
