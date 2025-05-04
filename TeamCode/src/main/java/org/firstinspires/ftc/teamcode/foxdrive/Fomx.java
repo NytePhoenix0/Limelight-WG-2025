1d9c5caa52bcf94131b2ad8ff8e831037c820914
Xendy <xendyexe@gmail.com>
2025-05-04 10:42:17 -0700

    FOMX

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/foxdrive/Fomx.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/foxdrive/Fomx.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.foxdrive;
 
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -23,6 +26,11 @@ public class Fomx extends LinearOpMode {
     public static double THRESHOLD = 0.1;
     public static double SPEED = 3000;
     public static double ROTATION_SPEED = 1000;
+
+    public static double WHEEL_RADIUS = 2.5;
+    public static double GEAR_RATIO = 0;
+    public static double TICKS_PER_REV = 0;
+    private double DISTANCE_PER_TICK = (2 * Math.PI * GEAR_RATIO * WHEEL_RADIUS)/TICKS_PER_REV;
 
     int targetIndex = 0;
     private double[][] targetPositions = {
@@ -82,7 +90,24 @@ public class Fomx extends LinearOpMode {
                 telemetry.addData("Current Position", String.format("(%s, %s)", x, y));
             } else {
                 // do stupid feedforward stuff here later
-                telemetry.addLine("No apriltags seen!!");
+                telemetry.addLine("No apriltags seen!! falling back to encoder odometry...");
+                double ticksLF = chassis.leftFront.getCurrentPosition() - currentTickLF;
+                double ticksLR = chassis.leftRear.getCurrentPosition() - currentTickLR;
+                double ticksRF = chassis.rightFront.getCurrentPosition() - currentTickRF;
+                double ticksRR = chassis.rightRear.getCurrentPosition() - currentTickRR;
+                double leftFrontMeters = ticksLF * DISTANCE_PER_TICK;
+                double rightFrontMeters = ticksRF * DISTANCE_PER_TICK;
+                double leftRearMeters = ticksLR * DISTANCE_PER_TICK;
+                double rightRearMeters = ticksRR * DISTANCE_PER_TICK;
+                currentTickLF = chassis.leftFront.getCurrentPosition();
+                currentTickLR = chassis.leftRear.getCurrentPosition();
+                currentTickRF = chassis.rightFront.getCurrentPosition();
+                currentTickRR = chassis.rightRear.getCurrentPosition();
+                double deltaY = (leftFrontMeters + rightFrontMeters + leftRearMeters + rightRearMeters) / 4.0;
+                double deltaX = (leftFrontMeters - rightFrontMeters - leftRearMeters + rightRearMeters) / 4.0;
+                x += deltaX * cos(yawRads) - deltaY * sin(yawRads);
+                y += deltaX * sin(yawRads) + deltaY * cos(yawRads);
+                telemetry.addData("Predicted Position", String.format("(%s, %s)", x, y));
             }
             double distX = targetX - x;
             double distY = targetY - y;
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/foxdrive/OdometryTesting.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode.foxdrive;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.limemode.LimelightOpMode;
+
+@TeleOp(name="OdometryTesting")
+public class OdometryTesting extends LimelightOpMode {
+    public static double MOTOR_SPEED = 2681.28;
+    private final static int[] validIDs = {11,12,13,14,15,16};
+    FtcDashboard dashboard;
+    IMU control_hub_imu;
+    boolean a = false;
+    @Override
+    public void start() {
+        leftFront = getDriveMotor("leftFront");
+        leftRear = getDriveMotor("leftRear");
+        rightFront = getDriveMotor("rightFront");
+        rightRear = getDriveMotor("rightRear");
+
+        control_hub_imu = hardwareMap.get(IMU.class, "imu2");
+        control_hub_imu.resetYaw();
+    }
+
+    @Override
+    public void update() {
+        if (!a) {
+            resetRuntime();
+            a = true;
+        }
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        YawPitchRollAngles orientation_control = control_hub_imu.getRobotYawPitchRollAngles();
+
+        telemetry.addData("Yaw", orientation.getYaw(AngleUnit.DEGREES));
+        telemetry.addData("Yaw vel", imu.getRobotAngularVelocity(AngleUnit.DEGREES).zRotationRate);
+        telemetry.addData("Control Yaw", orientation_control.getYaw(AngleUnit.DEGREES));
+        telemetry.addData("Control Yaw vel", control_hub_imu.getRobotAngularVelocity(AngleUnit.DEGREES).zRotationRate);
+        telemetry.addData("Runtime", getRuntime());
+
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+        double yawRads = this.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+        double rotX = x * Math.cos(-yawRads) - y * Math.sin(-yawRads);
+        double rotY = x * Math.sin(-yawRads) + y * Math.cos(-yawRads);
+        rotX *= 1.1;
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
+        telemetry.addData("yawRags", yawRads);
+        leftFront.setVelocity(frontLeftPower * MOTOR_SPEED);
+        leftRear.setVelocity(backLeftPower * MOTOR_SPEED);
+        rightFront.setVelocity(frontRightPower * MOTOR_SPEED);
+        rightRear.setVelocity(backRightPower * MOTOR_SPEED);
+        telemetry.update();
+    }
+
+    public DcMotorEx getDriveMotor(String name) {
+        DcMotorEx motor = (DcMotorEx) hardwareMap.get(DcMotor.class, name);
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        return motor;
+    }
+}
